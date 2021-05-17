89f049149f3b56b20b0d3ff5cf8143f6bda3a3eb
Emil John Lopez <emiljohn24@gmail.com>
2021-05-17 17:20:26 +0800

    FUCK

--- CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/xsim_webtalk.tcl
+++ CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir D:/Learning/LBYCPD3/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/
+webtalk_init -webtalk_dir D:/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon May 17 17:03:34 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon May 17 17:19:57 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "ef62866c-2441-4666-8944-c4e7e7493c28" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "289434e2e2ac403fae3bc18eb19e3e33" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "339" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "342" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "3" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.08_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6616_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3315095112 -regid "" -xml D:/Learning/LBYCPD3/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Learning/LBYCPD3/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Learning/LBYCPD3/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6568_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2276429432 -regid "" -xml D:/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/SAP-1/CPD3_Ex4_SAP.sim/sim_1/behav/xsim/xsim.dir/SAP1_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
